ba5f9589aadfb4f9646e3c690fe7b21d73dbd1d8
Mocha <70185768+MochaCoo@users.noreply.github.com>
2026-09-15 15:40:05 +0800

    Increase ITEM_LABEL_LENGTH to 8192

--- epan/proto.h
+++ epan/proto.h
@@ -48,7 +48,7 @@ extern "C" {
 WS_DLL_PUBLIC int hf_text_only;
 
 /** the maximum length of a protocol field string representation */
-#define ITEM_LABEL_LENGTH       240
+#define ITEM_LABEL_LENGTH       0x2000
 
 #define ITEM_LABEL_UNKNOWN_STR  "Unknown"
 
